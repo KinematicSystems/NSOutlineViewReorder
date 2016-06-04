393a415147c4b65077939cedc07c21b04b5eed6d
Matt Grippaldi <mgrippaldi@eoir.com>
2016-06-04 13:05:02 -0400

    Initial Commit

---
+++ OutlineViewReorder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OutlineViewReorder
+//
+//  Created by Matt Grippaldi on 6/4/16.
+//  Copyright © 2016 Kinematic Systems. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OutlineViewReorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutlineViewReorder
+//
+//  Created by Matt Grippaldi on 6/4/16.
+//  Copyright © 2016 Kinematic Systems. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
